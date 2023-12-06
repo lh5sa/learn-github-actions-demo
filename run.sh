df5a8a926e8e2ce81e2801d90df9de1d9f0aa217
liaohui5 <liaohui5@foxmail.com>
2023-12-07 06:18:21 +0800

    fix run.sh errors

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ docker rm $docker_container_name
 # 启动容器
 docker run -d \
 	-p 80:80 \
-	-v $(pwd)/dist:/usr/share/nginx/html \
-	--name $docker_container_name $image_name
+	-v $(pwd)/docs/.vitepress/dist:/usr/share/nginx/html \
+	--name $docker_container_name $docker_image_name
