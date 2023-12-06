181f14870ca2e4327034c616d3d930dc7da0675b
liaohui5 <liaohui5@foxmail.com>
2023-12-07 05:05:52 +0800

    add deploy to server workflow

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker_container_name="learn-github-action-demo"
+docker_image_name="nginx:stable"
+
+# 先停止原来的
+docekr stop $container_name
+docker rm $container_name
+
+# 启动容器
+docker run -d \
+	-p 80:80 \
+	-v $(pwd)/dist:/usr/share/nginx/html \
+	--name $docker_container_name $image_name
