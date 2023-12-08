0e4caa2e78422f1e86daae526da917c9909eaa09
liaohui5 <liaohui5@foxmail.com>
2023-12-09 02:35:11 +0800

    update vitepress base user option for deploy to server

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/learn-github-actions-demo/",
+  base: "/",
   title: "Learn Github Actions Demo",
   description: "A VitePress Site",
   themeConfig: {
